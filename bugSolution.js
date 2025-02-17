2ed3e4350c0354f663a037366f768108f30b6c66
BugHunter-X <askzenai@gmail.com>
2025-02-18 00:16:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return null; 
+  }
+  if(a === 0 || b === 0){
+    return a + b;
+  }
+  return a + b; 
+}
+
+console.log(foo(1, 2)); // Output: 3
+console.log(foo(null, 2)); // Output: null
+console.log(foo(1, null)); //Output: null
+console.log(foo(null, null)); //Output: null
+console.log(foo(0,2)); // Output: 2
+console.log(foo(2,0)); // Output: 2
